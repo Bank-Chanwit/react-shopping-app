dbd075b5f8e6f31ff0cefc6e0f3fa086c5eedc5d
Bank-Chanwit <ssamsungj7299@gmail.com>
2026-03-04 14:53:39 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css"
+export default function Header(){
+    return(
+        <header>
+            <p>Shopping App</p>
+            <p>สินค้าในตระกล้า : </p>
+        </header>
+    )
+}
