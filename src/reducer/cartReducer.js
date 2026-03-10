1172867300877a1a8af20f112b634e5056bd8ee9
Bank-Chanwit <ssamsungj7299@gmail.com>
2026-03-11 00:00:42 +0700

    add reducer

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -1,5 +1,25 @@
-const cartReducer = (state,action)=>{
-    //จัดการ state ผ่าน action
+const cartReducer = (state, action) => {
+  //กระบวนการจัดการ state ผ่าน action
+  if (action.type === "CALCULATE_TOTAL") {
+    const { total, amount } = state.products.reduce(
+      (cartTotal, item) => {
+        const { price, quantity } = item;
+        const totalprice = price * quantity; //ยอดรวมสินค้าแต่ละรายการ
+        cartTotal.total += totalprice; //จำนวนเงินรวม
+        cartTotal.amount += quantity; // ปริมาณรวม
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      },
+    );
+    return {
+      ...state,
+      total,
+      amount,
+    };
+  }
+};
 
-}
 export default cartReducer;
